cca2a45e4b0fc0f95b4914bdd7365c27f6c223ec
shanedabes <shane@isda.best>
2021-10-17 01:21:39 +0100

    handle autoreconnect

--- main.go
+++ main.go
@@ -19,7 +19,9 @@ type Options struct {
 }
 
 const (
-	mqttConnectRetryInternal = 5 * time.Second
+	moduleName               = "jod"
+	mqttConnectRetryInternal = 5
+	mqttDisconnectTimeout    = 1000
 )
 
 func jodHandler(m gowon.Message) (string, error) {
@@ -32,30 +34,61 @@ func jodHandler(m gowon.Message) (string, error) {
 	return out, nil
 }
 
-func main() {
-	opts := Options{}
+func defaultPublishHandler(c mqtt.Client, msg mqtt.Message) {
+	log.Printf("unexpected message:  %s\n", msg)
+}
 
-	_, err := flags.Parse(&opts)
-	if err != nil {
+func onConnectionLostHandler(c mqtt.Client, err error) {
+	log.Println("connection to broker lost")
+}
+
+func onRecconnectingHandler(c mqtt.Client, opts *mqtt.ClientOptions) {
+	log.Println("attempting to reconnect to broker")
+}
+
+func onConnectHandler(c mqtt.Client) {
+	log.Println("connected to broker")
+}
+
+func main() {
+	log.Printf("%s starting\n", moduleName)
+
+	opts := Options{}
+	if _, err := flags.Parse(&opts); err != nil {
 		log.Fatal(err)
 	}
 
-	mqttOpts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
-	mqttOpts.SetClientID("gowon_jokeoftheday")
+	mqttOpts := mqtt.NewClientOptions()
+	mqttOpts.AddBroker(fmt.Sprintf("tcp://%s", opts.Broker))
+	mqttOpts.SetClientID(fmt.Sprintf("gowon_%s", moduleName))
 	mqttOpts.SetConnectRetry(true)
-	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal)
+	mqttOpts.SetConnectRetryInterval(mqttConnectRetryInternal * time.Second)
+	mqttOpts.SetAutoReconnect(true)
+
+	mqttOpts.DefaultPublishHandler = defaultPublishHandler
+	mqttOpts.OnConnectionLost = onConnectionLostHandler
+	mqttOpts.OnReconnecting = onRecconnectingHandler
+	mqttOpts.OnConnect = onConnectHandler
+
+	mr := gowon.NewMessageRouter()
+	mr.AddCommand("jod", jodHandler)
+	mr.Subscribe(mqttOpts, moduleName)
+
+	log.Print("connecting to broker")
 
 	c := mqtt.NewClient(mqttOpts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	mr := gowon.NewMessageRouter()
-	mr.AddCommand("jod", jodHandler)
-	mr.Subscribe(c, "gowon-jokeoftheday")
+	log.Print("connected to broker")
 
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	<-sigs
+
+	log.Println("signal caught, exiting")
+	c.Disconnect(mqttDisconnectTimeout)
+	log.Println("shutdown complete")
 }
